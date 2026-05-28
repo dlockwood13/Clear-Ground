2dad3d7b27122c08e37716e60e8505c7c3550cea
Dan Lockwood <92036309+dlockwood13@users.noreply.github.com>
2026-05-28 11:31:07 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,32 @@
+const C = "clear-ground-v1";
+const ASSETS = ["./", "./index.html"];
 
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(C).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== C).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(cached =>
+      cached ||
+      fetch(e.request)
+        .then(res => {
+          const copy = res.clone();
+          caches.open(C).then(c => c.put(e.request, copy));
+          return res;
+        })
+        .catch(() => caches.match("./index.html"))
+    )
+  );
+});
